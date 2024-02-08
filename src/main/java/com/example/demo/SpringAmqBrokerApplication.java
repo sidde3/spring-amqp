e5f8be3f515b7343e401296641e71f94614e7cdd
Siddhartha De <sidde@redhat.com>
2024-02-08 10:15:54 +0800

    Multi-threading test

--- src/main/java/com/example/demo/SpringAmqBrokerApplication.java
+++ src/main/java/com/example/demo/SpringAmqBrokerApplication.java
@@ -8,27 +8,37 @@ import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 
+import java.util.UUID;
+
 @SpringBootApplication
 @EnableJms
 public class SpringAmqBrokerApplication implements CommandLineRunner {
-	@Autowired
-	private JmsTemplate jmsTemplate;
+    @Autowired
+    private JmsTemplate jmsTemplate;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAmqBrokerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAmqBrokerApplication.class, args);
+    }
 
-	@Override
-	public void run(String... strings) throws Exception {
-		sendMessage("Hello World!");
-	}
-	public void sendMessage(String text) {
-		System.out.println(String.format("Sending '%s'", text));
-		this.jmsTemplate.convertAndSend("ocbc-queue", text);
-	}
+    @Override
+    public void run(String... strings) throws Exception {
+        while (true) {
+            sendMessage();
+        }
+    }
 
-	@JmsListener(destination = "ocbc-queue" )
-	public void receiveMessage(String text) {
-		System.out.println(String.format("Received '%s'", text));
-	}
+    public void sendMessage() {
+		new Thread(() -> {
+            String text = UUID.randomUUID().toString();
+            System.out.println(String.format("Sending '%s'", text));
+            this.jmsTemplate.convertAndSend("test-queue", text);
+        }
+		).start();
+
+    }
+
+    @JmsListener(destination = "test-queue")
+    public void receiveMessage(String text) {
+        System.err.println(String.format("Received '%s'", text));
+    }
 }
